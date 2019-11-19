c7457bf499bf0b787ac4bbf0627508c0d08af23d
Brian Harring <brian@cloudflare.com>
2019-11-19 15:55:19 -0800

    Log sensitive data to debug log only.
    INFO_LOG shouldn't log user credentials, but the previous code was doing this.
    
    Instead, split up the logging so there is that level of visibility in debug mode,
    info for EAP cert related things, and AUTH for general logging flow.

--- authenticators/okta.py
+++ authenticators/okta.py
@@ -6,11 +6,19 @@
 
 import requests
 import radiusd
+import functools
 import os
+
+def _log(level, message, *args, **kwargs):
+  radiusd.radlog(level, "Okta Authenticate: {}".format(message.format(*args, **kwargs)))
+
+debug_log = functools.partial(_log, radiusd.L_DBG)
+auth_log = functools.partial(_log, radiusd.L_AUTH)
+info_log = functools.partial(_log, radiusd.L_INFO)
 
 
 def authenticate(p):
-  radiusd.radlog(radiusd.L_INFO, '*** Authenticate: {} ***'.format(p))
+  debug_log('Authenticate: parameters: {}', p)
   attributes = dict(p)
   username = attributes['User-Name']
   password = attributes['User-Password']
@@ -18,34 +26,35 @@ def authenticate(p):
   if not username.endswith('@{}'.format(os.environ['OKTA_DOMAIN'])):
     username = username + '@{}'.format(os.environ['OKTA_DOMAIN'])
 
-  radiusd.radlog(radiusd.L_INFO, '*** Authenticating: {} ***'.format(username))
+  auth_log('Authenticating: {}', username)
 
   url = 'https://{}/api/v1/authn'.format(os.environ['OKTA_ORG'])
   headers = {'Authorization': 'SSWS {}'.format(os.environ['OKTA_APITOKEN'])}
   payload = {'username': username, 'password': password}
   r = requests.post(url, json=payload, headers=headers)
 
-  if r.status_code == 200:
-    return radiusd.RLM_MODULE_OK
-  else:
-    return radiusd.RLM_MODULE_REJECT
+  result = radiusd.RLM_MODULE_OK if r.status_code == 200 else radiusd.RLM_MODULE_REJECT
+  debug_log('Authentication result: status={}, result={}', r.status_code, result)
+  return result
 
 
 def authorize(p):
-  radiusd.radlog(radiusd.L_INFO, '*** Authorize: {} ***'.format(p))
+  debug_log('Authorize: {}', p)
+  auth_log('Authorize: {}', dict(p)['User-Name'])
   return radiusd.RLM_MODULE_OK
 
 
 def post_auth(p):
-  radiusd.radlog(radiusd.L_INFO, '*** Post Authentication: {} ***'.format(p))
+  debug_log('Post Authentication: parameters: {}', p)
   params = dict(i for i in p)
+  user = params['User-Name']
 
   if params['EAP-Type'] == 'TLS':
-    radiusd.radlog(radiusd.L_INFO, 'Processing a client certificate')
+    info_log('Post Authentication: user {}: Processing EAP TLS client certificate', user)
     return radiusd.RLM_MODULE_OK
 
   elif params['EAP-Type'] == 'GTC':
-    radiusd.radlog(radiusd.L_INFO, 'Processing a GTC client')
+    info_log('Post Authentication: user {}: Processing EAP GTC', user)
     # For dynamic VLAN...
     # if params['User-Name'] == 'ben.hecht':
     #   vlan = '1'
@@ -58,4 +67,5 @@ def post_auth(p):
     return radiusd.RLM_MODULE_OK
 
   elif params['EAP-Type'] == 'PEAP' or params['EAP-Type'] == 'TTLS':
+    info_log('Post Authentication: user {}: Processing EAP {}', user, params['EAP-Type'])
     return radiusd.RLM_MODULE_OK
