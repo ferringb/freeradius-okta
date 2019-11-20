df73482dd4e6e1ed0ad53818fd330d7eafdf897a
Brian Harring <brian@cloudflare.com>
2019-11-19 17:49:12 -0800

    Rewrite okta authenticator to capture errors and be class based.
    It should be possible to use different authenticator deploys, thus the
    class is rewritten to not directly rely on os.environ; from_env exists for
    that pathway, and also provides a way to use env prefixing for lookups from
    environment.
    
    In that world, you'd just wind up using a mod-config python shim that imports
    and instantiates an authenticator- either passing org/domain/apitoken, or loading
    those from the environment.
    
    Via that refactoring, the code will now fail if you don't supply the env variables
    it requires (for the default/original deploy pattern).
    
    Finally, this also has defensive decorators deployed to capture exceptions- radius
    doesn't log them on it's own, thus this intercepts, logs, and then lets the exception
    fly.

--- authenticators/okta.py
+++ authenticators/okta.py
@@ -10,62 +10,116 @@ import functools
 import os
 
 def _log(level, message, *args, **kwargs):
-  radiusd.radlog(level, "Okta Authenticate: {}".format(message.format(*args, **kwargs)))
+  if args or kwargs:
+    message = message.format(*args, **kwargs)
+  radiusd.radlog(level, "Okta Backend: {}".format(message))
 
-debug_log = functools.partial(_log, radiusd.L_DBG)
-auth_log = functools.partial(_log, radiusd.L_AUTH)
-info_log = functools.partial(_log, radiusd.L_INFO)
+def capture_error(func):
+  def f(*args, **kwargs):
+    _log(radiusd.L_DBG, "invoking {}: args={!r}, kwargs={!r}".format(func.__name__, args, kwargs))
+    try:
+      ret = func(*args, **kwargs)
+      _log(radiusd.L_DBG, "invoking {}: result={}".format(func.__name__, ret))
+      return ret
+    except Exception as e:
+      _log(radiusd.L_ERR, "{} threw an exception: {}".format(func.__name__, e))
+      raise
+  f.__name__ = func.__name__
+  return f
+
+class OktaAuthenticator:
+
+  """
+  Freeradius okta authenticator.
+  """
+
+  @classmethod
+  @capture_error
+  def from_env(cls, prefix=None):
+    """Create an authenticator from environment variables
+
+    :param env_prefix: If given, look for ${PREFIX}_OKTA.* variables.  If not given, just look for OKTA_.*
+    """
+    def f(key, prefix='{}_'.format(prefix) if prefix is not None else ''):
+      key = '{}OKTA_{}'.format(prefix, key)
+      try:
+        return os.environ[key]
+      except KeyError:
+        raise KeyError("environment variable {} isn't defined, but is mandatory".format(key))
+    return cls(domain=f('DOMAIN'), org=f('ORG'), apitoken=f('APITOKEN'))
+
+  def __init__(self, domain, org, apitoken):
+    self.domain = domain
+    self.org = org
+    self.apitoken = apitoken
+
+  def debug_log(self, *args, **kwargs):
+    return _log(radiusd.L_DBG, *args, **kwargs)
+
+  def auth_log(self, *args, **kwargs):
+    return _log(radiusd.L_AUTH, *args, **kwargs)
+
+  def info_log(self, *args, **kwargs):
+    return _log(radiusd.L_INFO, *args, **kwargs)
+
+  @capture_error
+  def authenticate(self, p):
+    attributes = dict(p)
+    username = attributes['User-Name']
+    password = attributes['User-Password']
+
+    if not username.endswith('@{}'.format(self.domain)):
+      username = username + '@{}'.format(self.domain)
+
+    self.auth_log('Authenticating: {}', username)
+
+    url = 'https://{}/api/v1/authn'.format(self.org)
+    headers = {'Authorization': 'SSWS {}'.format(self.apitoken)}
+    payload = {'username': username, 'password': password}
+    r = requests.post(url, json=payload, headers=headers)
+
+    result = radiusd.RLM_MODULE_OK if r.status_code == 200 else radiusd.RLM_MODULE_REJECT
+    self.debug_log('Authentication result: status={}, result={}', r.status_code, result)
+    return result
+
+  @capture_error
+  def authorize(self, p):
+    self.auth_log('Authorize: {}', dict(p)['User-Name'])
+    return radiusd.RLM_MODULE_OK
+
+  @capture_error
+  def post_auth(self, p):
+    params = dict(i for i in p)
+    user = params['User-Name']
+
+    if params['EAP-Type'] == 'TLS':
+      self.info_log('Post Authentication: user {}: Processing EAP TLS client certificate', user)
+      return radiusd.RLM_MODULE_OK
+
+    elif params['EAP-Type'] == 'GTC':
+      self.info_log('Post Authentication: user {}: Processing EAP GTC', user)
+      # For dynamic VLAN...
+      # if params['User-Name'] == 'ben.hecht':
+      #   vlan = '1'
+      # return (radiusd.RLM_MODULE_OK,
+      #         (
+      #           ('Tunnel-Private-Group-Id', vlan),
+      #           ('Tunnel-Type', 'VLAN'),
+      #           ('Tunnel-Medium-Type', 'IEEE-802'),
+      #         ), ())
+      return radiusd.RLM_MODULE_OK
+
+    elif params['EAP-Type'] == 'PEAP' or params['EAP-Type'] == 'TTLS':
+      self.info_log('Post Authentication: user {}: Processing EAP {}', user, params['EAP-Type'])
+      return radiusd.RLM_MODULE_OK
 
 
+# compatibility shims for people directly using this module.
 def authenticate(p):
-  debug_log('Authenticate: parameters: {}', p)
-  attributes = dict(p)
-  username = attributes['User-Name']
-  password = attributes['User-Password']
-
-  if not username.endswith('@{}'.format(os.environ['OKTA_DOMAIN'])):
-    username = username + '@{}'.format(os.environ['OKTA_DOMAIN'])
-
-  auth_log('Authenticating: {}', username)
-
-  url = 'https://{}/api/v1/authn'.format(os.environ['OKTA_ORG'])
-  headers = {'Authorization': 'SSWS {}'.format(os.environ['OKTA_APITOKEN'])}
-  payload = {'username': username, 'password': password}
-  r = requests.post(url, json=payload, headers=headers)
-
-  result = radiusd.RLM_MODULE_OK if r.status_code == 200 else radiusd.RLM_MODULE_REJECT
-  debug_log('Authentication result: status={}, result={}', r.status_code, result)
-  return result
-
+  return OktaAuthenticator.from_env().authenticate(p)
 
 def authorize(p):
-  debug_log('Authorize: {}', p)
-  auth_log('Authorize: {}', dict(p)['User-Name'])
-  return radiusd.RLM_MODULE_OK
-
+  return OktaAuthenticator.from_env().authorize(p)
 
 def post_auth(p):
-  debug_log('Post Authentication: parameters: {}', p)
-  params = dict(i for i in p)
-  user = params['User-Name']
-
-  if params['EAP-Type'] == 'TLS':
-    info_log('Post Authentication: user {}: Processing EAP TLS client certificate', user)
-    return radiusd.RLM_MODULE_OK
-
-  elif params['EAP-Type'] == 'GTC':
-    info_log('Post Authentication: user {}: Processing EAP GTC', user)
-    # For dynamic VLAN...
-    # if params['User-Name'] == 'ben.hecht':
-    #   vlan = '1'
-    # return (radiusd.RLM_MODULE_OK,
-    #         (
-    #           ('Tunnel-Private-Group-Id', vlan),
-    #           ('Tunnel-Type', 'VLAN'),
-    #           ('Tunnel-Medium-Type', 'IEEE-802'),
-    #         ), ())
-    return radiusd.RLM_MODULE_OK
-
-  elif params['EAP-Type'] == 'PEAP' or params['EAP-Type'] == 'TTLS':
-    info_log('Post Authentication: user {}: Processing EAP {}', user, params['EAP-Type'])
-    return radiusd.RLM_MODULE_OK
+  return OktaAuthenticator.from_env().post_auth(p)
