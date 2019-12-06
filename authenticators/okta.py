26bf23b6a070d305916a4dc3ec624f9bc591b8e9
Brian Harring <brian@cloudflare.com>
2019-12-05 17:49:09 -0800

    Split and move some methods onto the class to make extension simpler.

--- authenticators/okta.py
+++ authenticators/okta.py
@@ -46,17 +46,6 @@ def capture_error(func):
   return f
 
 
-def _radius_response(value, reply=(), config=()):
-  """helper function for creating a properly formatted rlm_python response"""
-  if isinstance(reply, dict):
-    reply = reply.iteritems()
-  reply = tuple((str(k), str(v)) for k,v in reply)
-  if isinstance(config, dict):
-    config = config.iteritems()
-  config = tuple((str(k), str(v)) for k,v in config)
-  return (value, reply, config)
-
-
 class OktaAuthenticator:
   """
   Freeradius okta authenticator.
@@ -97,6 +86,17 @@ class OktaAuthenticator:
     self.auth_type = auth_type
     self.user_id_attr = user_id_attr
 
+  @staticmethod
+  def radius_response(value, reply=(), config=()):
+    """helper function for creating a properly formatted rlm_python response"""
+    if isinstance(reply, dict):
+      reply = reply.iteritems()
+    reply = tuple((str(k), str(v)) for k,v in reply)
+    if isinstance(config, dict):
+      config = config.iteritems()
+    config = tuple((str(k), str(v)) for k,v in config)
+    return (value, reply, config)
+
   def debug_log(self, *args, **kwargs):
     return _log(radiusd.L_DBG, *args, **kwargs)
 
@@ -131,9 +131,22 @@ class OktaAuthenticator:
     if r.status_code == 200:
       # if we can save the user-id in attr, do so- that saves an API roundtrip if they're doing MFA.
       if self.user_id_attr:
-        return _radius_response(radiusd.RLM_MODULE_OK, config={self.user_id_attr: data['_embedded']['user']['id']})
-      return _radius_response(radiusd.RLM_MODULE_OK)
-    return _radius_response(radiusd.RLM_MODULE_REJECT)
+        return self.radius_response(radiusd.RLM_MODULE_OK, config={self.user_id_attr: data['_embedded']['user']['id']})
+      return self.radius_response(radiusd.RLM_MODULE_OK)
+    return self.radius_response(radiusd.RLM_MODULE_REJECT)
+
+  def fetch_user_id(self, username):
+    self.debug_log("querying user id for user {}.  Consider enabling OKTA_USER_ID_ATTR to optimize this away", username)
+    response = self.okta_request('get', '/api/v1/users/{}'.format(username))
+    if response.status_code != 200:
+      # log the actual error via non debug at some point.
+      self.debug_log("non 200 status code: {!r}", response.json())
+      self.auth_log("user {} is unknown to okta", username)
+      return None
+
+    user_id = response.json()['id']
+    self.debug_log("user {} is user-id {}", username, user_id)
+    return user_id
 
   def authenticate_mfa(self, p):
     attributes = dict(p)
@@ -142,21 +155,21 @@ class OktaAuthenticator:
     user_id = attributes.get(self.user_id_attr)
 
     if user_id is None:
-      self.debug_log("MFA Authentication: querying user id for user {}.  Consider enabling OKTA_USER_ID_ATTR to optimize this away", username)
-      response = self.okta_request('get', '/api/v1/users/{}'.format(username))
-      if response.status_code != 200:
+      user_id = self.fetch_user_id(username)
+      if user_id is None:
         self.auth_log("MFA Authentication: user {} is unknown to okta", username)
-      user_id = response.json()['id']
-      self.debug_log("MFA Authentication: user {} is user-id {}", username, user_id)
+        return self.radius_response(radiusd.RLM_MODULE_REJECT)
 
     # get the factors allowed.
     response = self.okta_request('get', '/api/v1/users/{}/factors'.format(user_id))
     # this is a list of factors
     if response.status_code == 404:
       self.info_log("MFA Authentication: user {} not found", username)
-      return (radiusd.RLM_MODULE_REJECT, (('Reply-Message', 'user not found'),),)
+      return self.radius_response(radiusd.RLM_MODULE_REJECT, {'Reply-Message', 'user not found'})
+
     elif response.status_code != 200:
       self.warn_log("MFA Authentication: unexpected status code: {}, user-id {}", response.status_code, username)
+
     factors = response.json()
     self.debug_log("MFA Authentication: {} factors usable", len(factors))
     for factor in factors:
@@ -173,7 +186,13 @@ class OktaAuthenticator:
         self.info_log("MFA Authentication: validated {} token type {}", username, factor['factorType'])
         return radiusd.RLM_MODULE_OK
       else:
-        self.debug_log("MFA Authentication: factor auth failed: {!r}", r.json())
+        data = r.json()
+        self.debug_log("MFA Authentication: factor auth failed: {!r}", data)
+        if data['errorCode'] == 'E0000082':
+          self.auth_log(
+              "MFA Authentication: user {}'s token was used too recently: {}",
+              username, data['errorSummary'])
+          return self.radius_response(radiusd.RLM_MODULE_REJECT, {'Reply-Message': data['errorSummary']})
         self.auth_log("MFA Authentication: user {}, factor type {} failed auth; may be innocuous", username, factor["factorType"])
         # status 403
         # this can be: errorCode E0000082 (errorSummary holds details to log)
@@ -188,23 +207,23 @@ class OktaAuthenticator:
         # }
         # finally, can also log errorId for traceability (should in general)
     self.debug_log("MFA Authentication: user {} exhausted all factor options", username)
-    return _radius_response(radiusd.RLM_MODULE_REJECT, {'Reply-Message':'no factor could be verified'})
+    return self.radius_response(radiusd.RLM_MODULE_REJECT, {'Reply-Message':'no factor could be verified'})
 
   def authorize(self, p):
     attributes = dict(p)
     username = attributes.get('User-Name')
     if username is not None:
       if re.match('^\w+([\+\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', username):
-        return _radius_response(radiusd.RLM_MODULE_OK, config={'Auth-Type': self.auth_type})
+        return self.radius_response(radiusd.RLM_MODULE_OK, config={'Auth-Type': self.auth_type})
       if self.default_email_domain:
         # it's not an email address and we have a default, so enforce ours and claim this auth for ourselves.
         self.debug_log('Authorizing: {} user lacks an email domain, applying the default of {}'.format(username, self.default_email_domain))
         username += '@{}'.format(self.default_email_domain)
-        return _radius_response(radiusd.RLM_MODULE_OK, config={'Auth-Type': self.auth_type, 'User-Name': username})
+        return self.radius_response(radiusd.RLM_MODULE_OK, config={'Auth-Type': self.auth_type, 'User-Name': username})
       self.debug_log("Authorize: {} we don't know what to do with", username)
-      return _radius_response(radiusd.RLM_MODULE_NOOP)
+      return self.radius_response(radiusd.RLM_MODULE_NOOP)
     self.debug_log("Authorize: no user name provided")
-    return _radius_response(radiusd.RLM_MODULE_NOOP)
+    return self.radius_response(radiusd.RLM_MODULE_NOOP)
 
 
 # This is the known hooks that Rlm_python can invoke; this list should be kept in sync
